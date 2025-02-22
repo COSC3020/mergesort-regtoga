c9bc8bf150b654be142f99b8b9940c9a73227e66
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2025-02-22 14:48:01 -0700

    got the code to work

--- code.js
+++ code.js
@@ -1,7 +1,3 @@
-//mergesort([10,9,8,7,6,5,4,3,2,1])
-
-//mergesort([5,4,3,2,1]);
-
 function merge(arr, L_bounds, R_bounds){
     let leftIndex = L_bounds[0];
     let rightIndex = R_bounds[0];
@@ -10,6 +6,7 @@ function merge(arr, L_bounds, R_bounds){
         
         if (arr[leftIndex] < arr[rightIndex]){
             leftIndex++
+            //counter2 += 1;
         } else{
             let temp = arr[leftIndex]; // save first one
             
@@ -17,9 +14,10 @@ function merge(arr, L_bounds, R_bounds){
                 let temp2 = arr[i+1]; // save the next one
                 arr[i+1] = temp; // move the previous value up one
                 temp = temp2;
+                //counter2 += 1;
             }
             arr[leftIndex] = temp;
-        
+            //update the bounds
             L_bounds[0] = L_bounds[0] + 1;
             L_bounds[1] = L_bounds[1] + 1;
             rightIndex++;
@@ -29,27 +27,41 @@ function merge(arr, L_bounds, R_bounds){
     return arr;
 }
 
-function mergeSort(array, bounds=[0, array.length-1]) {
-    //base case
-    if (array.length == 0) {
+function mergeSort(array) {
+    //counter = 0;
+    //counter2 = 0;
+    //counter3 = 0;
+    let n = array.length;
+    if (n <= 1) {
         return array;
-      }
-    if (bounds[0] == bounds[1]) {
-      return array;
     }
-  
-    const mid = Math.floor((bounds[0] + bounds[1]) / 2);
-    const leftbounds = [bounds[0], mid];
-    const rightbounds = [mid+1, bounds[1]];
-  
-    array = mergeSort(array, leftbounds)
-    array = mergeSort(array, rightbounds)
+    //because this grows with a size of 2 i think that this loop executes log_2(n) times
+    //stops looping if width is larger than the length of the array
+    for (let width = 1; width < n; width *= 2) { //crawl up the array with a size of initially 1 and merge, the size grows aswe merge more and more
+        //this one seems to grow 2x as fast as the previous loop so maby log_2(log_2(n))
+        //it stops looping if i is larger than the length of the array
+        for (let i = 0; i < n; i += 2 * width) {
+            let left = i;
+            let mid = Math.min(i + width - 1, n - 1);
+            let right = Math.min(i + 2 * width - 1, n - 1);
+            //counter += 1;
+            if (mid < right) {
+                //counter2 = 0;
+                merge(array, [left, mid], [mid + 1, right]);
+                //counter3 += counter2;
+                //console.log(counter + ": " + counter2);
+            }
+        }
+    }
 
-    array = merge(array, leftbounds, rightbounds);
 
+    //console.log("Final count = " + (counter3));
     return array;
 }
 
+//console.log(mergeSort([20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]));
+//console.log(mergeSort([40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]));
+//console.log(mergeSort([1,1,1,1,1,1,1,1,1,1,15,111,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,222,1,1,12,12,12,56789]))
 //console.log(mergeSort([10,9,8,7,6,5,4,3,2,1]));
 //console.log(mergeSort([10,2,1]));
 //console.log(mergeSort([]));
